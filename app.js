67efeb5075912058d1e6c8e156fa2a0649392149
JoaoBernardoVeiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2023-06-07 15:41:23 +0100

    Starting files

---
+++ app.js
@@ -0,0 +1,18 @@
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded(
+    {extended: true}
+));
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
