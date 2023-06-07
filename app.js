921cf41458308141887cfe69d2beb63d5070c27a
JoaoBernardoVeiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2023-06-07 15:46:24 +0100

    Getting Set Up

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ app.use(bodyParser.urlencoded(
     {extended: true}
 ));
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+
+
+
+
+
+
 
 
 
