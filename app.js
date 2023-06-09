38ab11be77c8df27667bcd005fd08ae67d88c923
JoaoBernardoVeiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2023-06-09 13:19:52 +0100

    Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -33,7 +34,7 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username})
     .then((foundUser) => {
@@ -41,9 +42,11 @@ app.post("/login", (req, res) => {
             res.send("User not Found");
         } else {
             if(foundUser) {
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     })
@@ -54,15 +57,20 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password),
-    })
 
-    newUser.save()
-    .then(() => {
-        res.render("secrets");        
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) =>{
+        
+        const newUser = new User({
+            email: req.body.username,
+            password: hash,
+        });
+    
+        newUser.save()
+        .then(() => {
+            res.render("secrets");        
+        });
     });
+
 });
 
 
